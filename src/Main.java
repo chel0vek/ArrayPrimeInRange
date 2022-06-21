7d51faeb4509c1ec101accd258d222aa7994a01d
chel0vek <56306564+chel0vek@users.noreply.github.com>
2022-06-21 12:23:10 +0300

    Final code, cleaned up code
    final code, cleaned it up.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -11,8 +10,10 @@ public class Main {
         System.out.println("Введите два числа");
         int a = input.nextInt();
         int b = input.nextInt();
+        int[] array = fillArrayWithPrimeNums(a, b);
         System.out.println("простые числа в промежутке от а до b " + Arrays.toString(fillArrayWithPrimeNums(a, b)));
-        System.out.println("простые индексы массива " + Arrays.toString(fillArrayWithPrimeIndexes(a, b)));
+        System.out.println("простые индексы массива " + Arrays.toString(fillArrayWithPrimeNums(0, array.length)));
+        System.out.println("sum = " + sumNums(a, b, array));
     }
 
     static boolean isPrimeNumber(int number) {
@@ -57,79 +58,17 @@ public class Main {
         return array;
     }
 
-    static boolean isPrimeIndex(int index) {
-        if (index <= 1) {
-            return false;
-        }
-        return indexDivider(index, divider);
-    }
-
-    static boolean indexDivider(int index, int divider) {
-        if (divider <= index) {
-            if (index % divider == 0) {
-                dividers++;
-            }
-            return indexDivider(index, ++divider);
-        }
-        if (dividers == 2) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    static int[] fillArrayWithPrimeIndexes(int start, int finish) {
-        int size = 0;
-        int index = 2;
+    static int sumNums(int start, int finish, int[] array) {
+        int sum = 0;
+        int indexTwo = 2;
         int[] array1 = fillArrayWithPrimeNums(start, finish);
         int length = array1.length;
-        System.out.println(length);
-        for (int i = start; i <= length; i++) {
-            dividers = 0;
-            if (isPrimeIndex(i)) {
-                size++;
-            }
-        }
-        int[] array = new int[size];
-        int indexZero = 0;
-        // облатсь в которой метод должен пройтись по первому массиву и найти его простые индексы.
-        for (int i = index; i <= length; i++) {
-            dividers = 0;
-            if (isPrimeIndex(i)) {
-                array[indexZero] = i;
-                indexZero++;
-            }
-        }
-        return array;
-    }
-    /*static int sumNums(int start, int finish) {
-        int index = 2;
-        int sum = 0;
-        int size = 0;
-        for (int i = start; i <= finish; i++) {
-            dividers = 0;
-            if(isPrimeNumber(i)) {
-                size++;
-            }
-        }
-        int[] arr = new int[size];
-        System.out.println(Arrays.toString(arr));
-        for (int i = start; i <= size; i++) {
-            dividers = 0;
-            if (isPrimeIndex(i)) {
-                arr[index] = i;
-                index++;
-            }
-        }
-        for (int i = start; i <= size; i++) {
-            dividers = 0;
-            if (isPrimeIndex(i)) {
-                if (start <= size) {
-                    sum = sum + arr[i];
-                    sumNums(start,finish);
+            for (int i = indexTwo; i <= length; i++) {
+                dividers = 0;
+                if (isPrimeNumber(i)) {
+                    sum += array[i];
                 }
             }
-        }
         return sum;
-    } */
+    }
 }
